fe45815f1df540dc376f3b195b326ab913eb5e63
Lucas Lima <limalucas454@gmail.com>
2021-04-27 03:47:45 +0000

    removing port from main.js

--- main.js
+++ main.js
@@ -11,20 +11,22 @@ const kayn = Kayn('RGAPI-4c985925-373c-456b-9e80-c6d71c812723')({
 })
 
 app.get('/champions', (req, res) => {
-    kayn.DDragon.Champion.list().then(function(champions){
+    kayn.DDragon.Champion.list().then(function (champions) {
         const champfilter = champions.data;
         let table = [];
-        for(champion in champfilter){
+        for (champion in champfilter) {
             championinfo = champfilter[champion];
-            table.push({name: championinfo.name, 
-                        tags: championinfo.tags,
-                        key: championinfo.key,
-                        square_image:  'http://ddragon.leagueoflegends.com/cdn/11.8.1/img/champion/' + championinfo.image.full,
-                        loading_image: 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + championinfo.id + '_0.jpg'})
+            table.push({
+                name: championinfo.name,
+                tags: championinfo.tags,
+                key: championinfo.key,
+                square_image: 'http://ddragon.leagueoflegends.com/cdn/11.8.1/img/champion/' + championinfo.image.full,
+                loading_image: 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + championinfo.id + '_0.jpg'
+            })
         }
         res.send(table)
     });
 });
 
-app.listen(5540);
+app.listen(process.env.PORT || 80);
 
