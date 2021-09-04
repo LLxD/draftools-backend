af55243bf3dc2a479ff66c75b6461980316304ea
Lucas Lima <limalucas454@gmail.com>
2021-09-04 13:22:07 -0300

    fixing hardcoded version and enving key

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const { request, response } = require('express');
 const express = require('express')
 const app = express();
 
 const { Kayn, REGIONS } = require('kayn')
 
-const kayn = Kayn('RGAPI-4c985925-373c-456b-9e80-c6d71c812723')({
+
+const kayn = Kayn(process.env.RIOT_LOL_API_KEY)({
     region: REGIONS.BRAZIL,
     apiURLPrefix: 'https://br1.api.riotgames.com/',
     locale: 'pt_BR',
@@ -20,12 +20,15 @@ app.get('/champions', (req, res) => {
                 name: championinfo.name,
                 tags: championinfo.tags,
                 key: championinfo.key,
-                square_image: 'http://ddragon.leagueoflegends.com/cdn/11.8.1/img/champion/' + championinfo.image.full,
+                square_image: 'http://ddragon.leagueoflegends.com/cdn/' + championinfo.version + '/img/champion/' + championinfo.image.full,
                 loading_image: 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + championinfo.id + '_0.jpg'
             })
         }
         res.send(table)
     });
+});
+app.get('/result', (req, res) => {
+    console.log(req);
 });
 
 app.listen(process.env.PORT || 80);
